29d008a00841dab98b61c408dd82d23bb61238c0
Lauren Morrow <morrowlauren17@gmail.com>
2019-02-26 20:50:16 -0800

    testing heroku live app res

--- server.js
+++ server.js
@@ -3,7 +3,22 @@
 
  const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
+// CORS FUNCTION
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
+
+ app.get("/", (req, res) => {
    res.json({ok: true});
  });
 
