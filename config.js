db58179b5b711da645ea83eddb08c2905c56b93a
Lauren Morrow <morrowlauren17@gmail.com>
2019-03-02 17:26:12 -0800

    local remote heroku and config file setup

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/shipwhen';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/shipwhen';
-exports.PORT = process.env.PORT || 8080;
+module.exports = {
+	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/shipwhen-api',
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-shipwhen-api',
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' ,
+	PORT: process.env.PORT || 8080,
+};
--- server.js
+++ server.js
@@ -1,22 +1,63 @@
  const express = require('express');
  const app = express();
+ const morgan = require('morgan');
+ const mongoose = require('mongoose');
  var cors = require('cors');
- const {CLIENT_ORIGIN} = require('./config');
 
-// My port is now in config.js, can delete this once i re-do line 20
- const PORT = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
+
+ const {PORT, DATABASE_URL, CLIENT_ORIGIN} = require('./config');
+
+app.use(morgan('common'));
+app.use(express.json());
+app.use(express.static("public"));
 
 // CORS FUNCTION
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+app.use(cors());
 
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
  });
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+ 
+let server;
 
- module.exports = {app};
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+      	.listen(port, function() {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', function(err) {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
