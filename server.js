a4a86364d47ca2cd44181aafe0448f3a78995787
Lauren Morrow <morrowlauren17@gmail.com>
2019-05-30 17:25:44 -0700

    deploy attempt 4

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'shipwhen-client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'shipwhen-client/build', 'index.html'));
   });
 }
  
