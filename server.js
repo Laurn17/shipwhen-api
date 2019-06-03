d35c3cb194444a3560d114d957eea23ee8959b06
Lauren Morrow <morrowlauren17@gmail.com>
2019-06-03 15:29:05 -0700

    final commit for now

--- server.js
+++ server.js
@@ -48,9 +48,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('/*', function(req, res) {
-      res.redirect("www.shipwhen.com");
-  });
-  app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
