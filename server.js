a6033e28fb41a2c3d467b2e1b5549492079e9361
Lauren Morrow <morrowlauren17@gmail.com>
2019-06-03 15:09:40 -0700

    trying redirect again

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const { PORT, DATABASE_URL } = require('./config');
 app.use(morgan('common'));
 
 app.use(express.json());
-app.all(forceDomain({
-  hostname: 'http://www.shipwhen.com'
+app.use(forceDomain({
+  hostname: 'https://www.shipwhen.com'
 }));
 // app.use(express.static("public"));
 
